2b503c34514f0b64c33ccee8f155d7c190f14f71
gaballard <geoffreyballard@gmail.com>
2022-04-07 15:25:42 -0700

    feat(Vectors): added Vec3 struct and associated methods/overloads

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
+use std::ops;
+
+#[derive(Debug)]
 struct Vec3 {
-    e: vec![0.0, 0.0, 0.0],
+    e: Vec<f32>,
 }
 
 impl Vec3 {
-    fn Vec3(&self, e0: f32, e1: f32, e2: f32) {
-        self.e = vec![e0, e1, e2];
-    }
-
     fn x(&self) -> f32 {
         self.e[0]
     }
@@ -17,73 +16,119 @@ impl Vec3 {
         self.e[2]
     }
 
-    fn negative(&self) -> &Vec3 {
-        self.e[0] = -self.e[0];
-        self.e[1] = -self.e[1];
-        self.e[2] = -self.e[2];
-        return self;
-    }
-
-    fn addTo(&self, v: Vec<f32>) -> &Vec3 {
-        self.e[0] += v[0];
-        self.e[1] += v[1];
-        self.e[2] += v[2];
-        return self;
-    }
-
-    fn multiplyTo(&self, t: f32) -> &Vec3 {
-        self.e[0] *= t;
-        self.e[1] *= t;
-        self.e[2] *= t;
-        return self;
-    }
-
-    fn divideTo(&self, t: f32) -> &Vec3 {
-        self.multiplyTo(1.0 / t);
-        return self;
-    }
-
-    fn lengthSquared(&self) -> f32 {
+    fn length_squared(&self) -> f32 {
         self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]
     }
-
     fn length(&self) -> f32 {
-        self.lengthSquared().sqrt()
+        self.length_squared().sqrt()
     }
+}
 
-    // Utils
+impl ops::Neg for Vec3 {
+    type Output = Vec3;
 
-    fn print(&self, v: Vec3) {
-        println!("{} {} {}", v.e[0], v.e[1], v.e[2]);
+    fn neg(self) -> Vec3 {
+        Vec3 {
+            e: vec![-self.e[0], -self.e[1], -self.e[2]],
+        }
     }
-    fn add(&self, u: Vec3, v: Vec3) {
-        self.Vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2])
+}
+
+impl ops::AddAssign for Vec3 {
+    fn add_assign(&mut self, v: Vec3) {
+        *self = Vec3 {
+            e: vec![self.e[0] + v.e[0], self.e[1] + v.e[1], self.e[2] + v.e[2]],
+        }
     }
-    fn subtract(&self, u: Vec3, v: Vec3) {
-        self.Vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2])
+}
+
+impl ops::SubAssign for Vec3 {
+    fn sub_assign(&mut self, v: Vec3) {
+        *self = Vec3 {
+            e: vec![self.e[0] - v.e[0], self.e[1] - v.e[1], self.e[2] - v.e[2]],
+        }
     }
-    fn multiply(&self, u: Vec3, v: Vec3) {
-        self.Vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2])
+}
+
+impl ops::MulAssign for Vec3 {
+    fn mul_assign(&mut self, v: Vec3) {
+        *self = Vec3 {
+            e: vec![self.e[0] * v.e[0], self.e[1] * v.e[1], self.e[2] * v.e[2]],
+        }
     }
-    fn multiplyBy(&self, t: f32, v: Vec3) {
-        self.Vec3(t * v.e[0], t * v.e[1], t * v.e[2])
+}
+
+impl ops::DivAssign for Vec3 {
+    fn div_assign(&mut self, v: Vec3) {
+        *self = Vec3 {
+            e: vec![self.e[0] / v.e[0], self.e[1] / v.e[1], self.e[2] / v.e[2]],
+        }
     }
-    fn divide(&self, v: Vec3, t: f32) -> &Vec3 {
-        v.multiplyTo(1.0 / t)
+}
+
+trait DotProduct {
+    fn dot(&self, v: Vec3) -> f32;
+}
+
+trait CrossProduct {
+    fn cross(&self, v: Vec3) -> Vec3;
+}
+
+impl DotProduct for Vec3 {
+    fn dot(&self, v: Vec3) -> f32 {
+        self.e[0] * v.e[0] + self.e[1] * v.e[1] + self.e[2] * v.e[2]
     }
-    fn dot(&self, u: Vec3, v: Vec3) {
-        u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2]
+}
+
+impl CrossProduct for Vec3 {
+    fn cross(&self, v: Vec3) -> Vec3 {
+        Vec3 {
+            e: vec![
+                self.e[1] * v.e[2] - self.e[2] * v.e[1],
+                self.e[2] * v.e[0] - self.e[0] * v.e[2],
+                self.e[0] * v.e[1] - self.e[1] * v.e[0],
+            ],
+        }
     }
-    fn cross(&self, u: Vec3, v: Vec3) {
-        self.Vec3(
+}
+
+fn print(v: &Vec3) {
+    println!("{} {} {}", v.e[0], v.e[1], v.e[2]);
+}
+fn add(u: &Vec3, v: &Vec3) -> Vec3 {
+    Vec3 {
+        e: vec![u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]],
+    }
+}
+fn subtract(u: &Vec3, v: &Vec3) -> Vec3 {
+    Vec3 {
+        e: vec![u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]],
+    }
+}
+fn multiply(u: &Vec3, v: &Vec3) -> Vec3 {
+    Vec3 {
+        e: vec![u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]],
+    }
+}
+fn divide(v: &Vec3, t: f32) -> Vec3 {
+    Vec3 {
+        e: vec![v.e[0] / t, v.e[1] / t, v.e[2] / t],
+    }
+}
+fn dot(u: &Vec3, v: &Vec3) -> f32 {
+    u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2]
+}
+fn cross(u: &Vec3, v: &Vec3) -> Vec3 {
+    Vec3 {
+        e: vec![
             u.e[1] * v.e[2] - u.e[2] * v.e[1],
             u.e[2] * v.e[0] - u.e[0] * v.e[2],
             u.e[0] * v.e[1] - u.e[1] * v.e[0],
-        );
+        ],
     }
-    fn unitVector(&self, v: Vec3) -> &Vec3 {
-        v.divide(v, v.length())
-    }
+}
+fn unit_vector(v: &Vec3) -> Vec3 {
+    divide(v, v.length())
 }
 
 // 3D Point
@@ -92,12 +137,12 @@ type Point3 = Vec3;
 // RGB Color
 type Color = Vec3;
 
-fn writeColor(pixelColor: Color) {
+fn write_color(pixel_color: Color) {
     println!(
         "{} {} {}",
-        (255.999 * pixelColor.x()) as u16,
-        (255.999 * pixelColor.y()) as u16,
-        (255.999 * pixelColor.z()) as u16
+        (255.999 * pixel_color.x()) as u16,
+        (255.999 * pixel_color.y()) as u16,
+        (255.999 * pixel_color.z()) as u16
     )
 }
 
@@ -112,24 +157,14 @@ fn main() {
     for y in (0..IMAGE_HEIGHT).rev() {
         eprint!("\rScanlines remaining: {} ", y);
         for x in 0..IMAGE_WIDTH {
-            let pixelColor: Color = Color {
+            let pixel_color: Color = Color {
                 e: vec![
                     x as f32 / (IMAGE_WIDTH as f32 - 1.0),
                     y as f32 / (IMAGE_HEIGHT as f32 - 1.0),
                     0.25,
                 ],
             };
-            writeColor(pixelColor);
-
-            // let r = x as f32 / (IMAGE_WIDTH as f32 - 1.0);
-            // let g = y as f32 / (IMAGE_HEIGHT as f32 - 1.0);
-            // let b: f32 = 0.25;
-
-            // let ir = (255.999 * r) as u16;
-            // let ig = (255.999 * g) as u16;
-            // let ib = (255.999 * b) as u16;
-
-            // println!("{} {} {}", ir, ig, ib);
+            write_color(pixel_color);
         }
     }
 
