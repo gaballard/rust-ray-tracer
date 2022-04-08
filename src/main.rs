a9c5f5dcbeecdaf37328975c15e418346b56f3da
gaballard <geoffreyballard@gmail.com>
2022-04-07 19:33:41 -0700

    feat(Normals): added colors based on surface normals and cleaned up code

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ impl Vector3 {
     fn length(&self) -> f32 {
         self.length_squared().sqrt()
     }
-    fn print(&self) {
+    fn _print(&self) {
         println!("{} {} {}", self.e[0], self.e[1], self.e[2]);
     }
 }
@@ -70,47 +70,11 @@ impl ops::Add for Vector3 {
     }
 }
 
-impl ops::Add for &Vector3 {
-    type Output = Vector3;
-    fn add(self, v: &Vector3) -> Vector3 {
-        Vector3 {
-            e: [self.e[0] + v.e[0], self.e[1] + v.e[1], self.e[2] + v.e[2]],
-        }
-    }
-}
-
-impl ops::Sub for &Vector3 {
-    type Output = Vector3;
-    fn sub(self, v: &Vector3) -> Vector3 {
-        Vector3 {
-            e: [self.e[0] - v.e[0], self.e[1] - v.e[1], self.e[2] - v.e[2]],
-        }
-    }
-}
-
 impl ops::Sub for Vector3 {
     type Output = Vector3;
     fn sub(self, v: Vector3) -> Vector3 {
         Vector3 {
             e: [self.e[0] - v.e[0], self.e[1] - v.e[1], self.e[2] - v.e[2]],
-        }
-    }
-}
-
-impl ops::Mul for &Vector3 {
-    type Output = Vector3;
-    fn mul(self, v: &Vector3) -> Vector3 {
-        Vector3 {
-            e: [self.e[0] * v.e[0], self.e[1] * v.e[1], self.e[2] * v.e[2]],
-        }
-    }
-}
-
-impl ops::Mul for Vector3 {
-    type Output = Vector3;
-    fn mul(self, v: Vector3) -> Vector3 {
-        Vector3 {
-            e: [self.e[0] * v.e[0], self.e[1] * v.e[1], self.e[2] * v.e[2]],
         }
     }
 }
@@ -142,24 +106,6 @@ impl ops::Mul<Vector3> for f32 {
     }
 }
 
-impl ops::Mul<&Vector3> for f32 {
-    type Output = Vector3;
-    fn mul(self, v: &Vector3) -> Vector3 {
-        Vector3 {
-            e: [self * v.e[0], self * v.e[1], self * v.e[2]],
-        }
-    }
-}
-
-impl ops::Mul<&Vector3> for &f32 {
-    type Output = Vector3;
-    fn mul(self, v: &Vector3) -> Vector3 {
-        Vector3 {
-            e: [self * v.e[0], self * v.e[1], self * v.e[2]],
-        }
-    }
-}
-
 impl ops::Div<f32> for &Vector3 {
     type Output = Vector3;
     fn div(self, t: f32) -> Vector3 {
@@ -177,10 +123,10 @@ impl ops::Div<f32> for Vector3 {
 /**
  * Vector Utils
  */
-fn dot(u: &Vector3, v: &Vector3) -> f32 {
+fn _dot(u: &Vector3, v: &Vector3) -> f32 {
     u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2]
 }
-fn cross(u: &Vector3, v: &Vector3) -> Vector3 {
+fn _cross(u: &Vector3, v: &Vector3) -> Vector3 {
     Vector3 {
         e: [
             u.e[1] * v.e[2] - u.e[2] * v.e[1],
@@ -189,7 +135,7 @@ fn cross(u: &Vector3, v: &Vector3) -> Vector3 {
         ],
     }
 }
-fn unit_vector(v: &Vector3) -> Vector3 {
+fn _unit_vector(v: &Vector3) -> Vector3 {
     v / v.length()
 }
 
@@ -212,17 +158,27 @@ fn write_color(pixel_color: Color) {
     )
 }
 fn ray_color(r: Ray) -> Color {
-    if hit_sphere(
+    let mut t = hit_sphere(
         &Point3 {
             e: [0.0, 0.0, -1.0],
         },
         0.5,
         &r,
-    ) {
-        return Color { e: [1.0, 0.0, 0.0] };
+    );
+    if t > 0.0 {
+        let normal = _unit_vector(
+            &(r._at(t)
+                - Vector3 {
+                    e: [0.0, 0.0, -1.0],
+                }),
+        );
+        return 0.5
+            * Color {
+                e: [normal.x() + 1.0, normal.y() + 1.0, normal.z() + 1.0],
+            };
     }
-    let unit_direction = unit_vector(&r.direction());
-    let t: f32 = 0.5 * (unit_direction.y() + 1.0);
+    let unit_direction = _unit_vector(&r.direction());
+    t = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - t) * Color { e: [1.0, 1.0, 1.0] } + t * Color { e: [0.5, 0.7, 1.0] }
 }
 
@@ -242,18 +198,22 @@ impl Ray {
         self.dir
     }
 
-    fn at(&self, t: f32) -> Point3 {
+    fn _at(&self, t: f32) -> Point3 {
         self.orig + (t * self.dir)
     }
 }
 
-fn hit_sphere(center: &Point3, radius: f32, r: &Ray) -> bool {
+fn hit_sphere(center: &Point3, radius: f32, r: &Ray) -> f32 {
     let oc = r.origin() - *center;
-    let a = dot(&r.direction(), &r.direction());
-    let b = 2.0 * dot(&oc, &r.direction());
-    let c = dot(&oc, &oc) - radius * radius;
+    let a = _dot(&r.direction(), &r.direction());
+    let b = 2.0 * _dot(&oc, &r.direction());
+    let c = _dot(&oc, &oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c; // quadraticc equationz
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        return -1.0;
+    } else {
+        return (-b - discriminant.sqrt()) / (2.0 * a);
+    }
 }
 
 /**
@@ -263,7 +223,7 @@ fn main() {
     // Image
     const ASPECT_RATIO: f32 = 16.0 / 9.0;
 
-    const IMAGE_WIDTH: u16 = 400;
+    const IMAGE_WIDTH: u16 = 1280;
     const IMAGE_HEIGHT: u16 = (IMAGE_WIDTH as f32 / ASPECT_RATIO) as u16;
 
     // Camera
@@ -285,11 +245,15 @@ fn main() {
             e: [0.0, 0.0, focal_length],
         };
 
-    // Render
+    // Renderer
     println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
 
     for y in (0..IMAGE_HEIGHT).rev() {
-        eprint!("\rScanlines remaining: {} ", y);
+        eprint!(
+            "\rScanlines remaining: {} ({}%)",
+            y,
+            ((1.0 - (y as f32 / IMAGE_HEIGHT as f32)) * 100.0) as u16
+        );
         for x in 0..IMAGE_WIDTH {
             let u = x as f32 / (IMAGE_WIDTH as f32 - 1.0);
             let v = y as f32 / (IMAGE_HEIGHT as f32 - 1.0);
@@ -303,43 +267,4 @@ fn main() {
     }
 
     eprintln!("\nDone.");
-}
-
-fn tests() {
-    let mut a = Vector3 { e: [1.0, 1.0, 1.0] };
-
-    a.print();
-    assert_eq!(a.e, [1.0, 1.0, 1.0]);
-
-    a += Vector3 { e: [2.0, 2.0, 2.0] };
-    a.print();
-    assert_eq!(a.e, [3.0, 3.0, 3.0]);
-
-    a *= 3.0;
-    a.print();
-    assert_eq!(a.e, [9.0, 9.0, 9.0]);
-
-    a /= 3.0;
-    a.print();
-    assert_eq!(a.e, [3.0, 3.0, 3.0]);
-
-    let b = a + Vector3 { e: [2.0, 2.0, 2.0] };
-    b.print();
-    assert_eq!(b.e, [5.0, 5.0, 5.0]);
-    let c = b - Vector3 { e: [1.5, 1.5, 1.5] };
-    c.print();
-    assert_eq!(c.e, [3.5, 3.5, 3.5]);
-    let d = c * Vector3 { e: [2.0, 2.0, 2.0] };
-    d.print();
-    assert_eq!(d.e, [7.0, 7.0, 7.0]);
-    let e = d * 0.5;
-    e.print();
-    assert_eq!(e.e, [3.5, 3.5, 3.5]);
-    let f = e / 1.25;
-    f.print();
-    assert_eq!(f.e, [2.8, 2.8, 2.8]);
-    println!("{:?}", unit_vector(&f));
-    println!("{:?}", dot(&f, &Vector3 { e: [2.0, 2.0, 2.0] }));
-    f.print();
-    println!("{:?}", cross(&f, &Vector3 { e: [4.0, 4.0, 4.0] }));
 }
